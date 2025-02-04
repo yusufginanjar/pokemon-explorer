100cd9f03d118bcf5b84f3a957658210f123e4a4
yusufginanjar <yusufginanjar16@gmail.com>
2025-02-04 23:17:35 +0700

    style(Detail Pokemon): init style

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -4,13 +4,16 @@ import axios from "axios";
 import {
 	Container,
 	Card,
-	CardMedia,
 	CardContent,
 	Typography,
 	CircularProgress,
 	Box,
+	LinearProgress,
+	List,
+	ListItem,
 } from "@mui/material";
 import Grid from "@mui/material/Grid2";
+import styled from "@mui/material/styles/styled";
 
 // Interface for Pokémon details
 interface PokemonDetail {
@@ -38,26 +41,46 @@ interface PokemonDetail {
 	}[];
 }
 
+const CustomCard = styled(Card)(({ theme }) => ({
+	borderRadius: theme.spacing(1),
+	boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+}));
+
+const CustomBox = styled(Box)(({ theme }) => ({
+	border: "1px solid",
+	borderRadius: theme.spacing(1),
+	borderColor: "#e0e0e0",
+	backgroundColor: "#f5f5f5",
+	padding: theme.spacing(1),
+	textAlign: "center",
+}));
+
 // Helper functions for unit conversion
 const convertToMeter = (decimeter: number) => decimeter / 10;
 const convertToKg = (hectogram: number) => hectogram / 10;
 
 // Component to display Pokémon image
 const PokemonImage = ({ image, name }: { image: string; name: string }) => (
-	<Card sx={{ height: "100%" }}>
-		<CardMedia component="img" height="200" image={image} alt={name} />
-	</Card>
+	<CustomCard sx={{ height: "100%" }}>
+		<img src={image} alt={name} style={{ width: "100%", height: "auto" }} />
+	</CustomCard>
 );
 
 // Component to display Pokémon abilities
-const PokemonAbilities = ({ abilities }: { abilities: PokemonDetail["abilities"] }) => (
+const PokemonAbilities = ({
+	abilities,
+}: {
+	abilities: PokemonDetail["abilities"];
+}) => (
 	<Box display="flex" flexDirection="column">
 		<Typography variant="h6" gutterBottom>
 			Abilities
 		</Typography>
-		{abilities.map((ability, index) => (
-			<Typography key={`ability-${index}`}>{ability.ability.name}</Typography>
-		))}
+		<List dense>
+			{abilities.map((ability, index) => (
+				<ListItem key={`ability-${index}`}>{ability.ability.name}</ListItem>
+			))}
+		</List>
 	</Box>
 );
 
@@ -67,23 +90,36 @@ const PokemonTypes = ({ types }: { types: PokemonDetail["types"] }) => (
 		<Typography variant="h6" gutterBottom>
 			Types
 		</Typography>
-		{types.map((type, index) => (
-			<Typography key={`type-${index}`}>{type.type.name}</Typography>
-		))}
+		<List dense>
+			{types.map((type, index) => (
+				<ListItem key={`type-${index}`}>{type.type.name}</ListItem>
+			))}
+		</List>
 	</Box>
 );
 
 // Component to display Pokémon height and weight
-const PokemonPhysicalDetails = ({ height, weight }: { height: number; weight: number }) => (
+const PokemonPhysicalDetails = ({
+	height,
+	weight,
+}: {
+	height: number;
+	weight: number;
+}) => (
 	<Box>
-		<Typography variant="h6" gutterBottom>
+		<Typography variant="h6" gutterBottom align="center">
 			Height
 		</Typography>
-		<Typography>{convertToMeter(height)} m</Typography>
-		<Typography variant="h6" gutterBottom>
+		<CustomBox>
+			<Typography>{convertToMeter(height)} m</Typography>
+		</CustomBox>
+		<Box sx={{ my: 2 }} />
+		<Typography variant="h6" gutterBottom align="center">
 			Weight
 		</Typography>
-		<Typography>{convertToKg(weight)} kg</Typography>
+		<CustomBox>
+			<Typography>{convertToKg(weight)} kg</Typography>
+		</CustomBox>
 	</Box>
 );
 
@@ -94,9 +130,17 @@ const PokemonStats = ({ stats }: { stats: PokemonDetail["stats"] }) => (
 			Stats
 		</Typography>
 		{stats.map((stat, index) => (
-			<Box key={`stat-${index}`} display="flex" justifyContent="space-between">
-				<Typography>{stat.stat.name}</Typography>
-				<Typography>{stat.base_stat}</Typography>
+			<Box key={`stat-${index}`} sx={{ mb: 2 }}>
+				<Box display="flex" justifyContent="space-between">
+					<Typography variant="body1">{stat.stat.name}</Typography>
+					<Typography variant="body1">{stat.base_stat}</Typography>
+				</Box>
+				<LinearProgress
+					variant="determinate"
+					color={stat.base_stat > 100 ? "warning" : "info"}
+					value={stat.base_stat > 100 ? 100 : stat.base_stat}
+					sx={{ height: 20, borderRadius: 5 }}
+				/>
 			</Box>
 		))}
 	</Box>
@@ -127,7 +171,12 @@ const PokemonDetail = () => {
 		fetchPokemon();
 	}, [id]);
 
-	if (loading) return <CircularProgress style={{ display: "block", marginTop: "50vh", marginLeft: "50vw" }} />;
+	if (loading)
+		return (
+			<CircularProgress
+				style={{ display: "block", marginTop: "50vh", marginLeft: "50vw" }}
+			/>
+		);
 	if (error) return <Typography color="error">{error}</Typography>;
 
 	return (
@@ -136,36 +185,47 @@ const PokemonDetail = () => {
 				<Grid container spacing={2}>
 					{/* Pokémon Image */}
 					<Grid size={{ xs: 12, md: 4 }}>
-						<PokemonImage image={pokemon.sprites.front_default} name={pokemon.name} />
+						<PokemonImage
+							image={pokemon.sprites.front_default}
+							name={pokemon.name}
+						/>
 					</Grid>
 
 					{/* Pokémon Details */}
 					<Grid size={{ xs: 12, md: 8 }}>
-						<Card sx={{ height: "100%" }}>
+						<CustomCard sx={{ height: "100%", p: 2 }}>
 							<CardContent>
-								<Typography variant="h4" gutterBottom align="left">
+								<Typography
+									variant="h4"
+									gutterBottom
+									align="center"
+									className="font-pokemon-hollow">
 									{pokemon.name.toUpperCase()}
 								</Typography>
-								<Grid container spacing={2}>
+								<Grid container spacing={2} sx={{ mt: 4 }}>
 									<Grid size={6}>
 										<PokemonAbilities abilities={pokemon.abilities} />
+										<Box sx={{ my: 2 }} />
 										<PokemonTypes types={pokemon.types} />
 									</Grid>
 									<Grid size={6}>
-										<PokemonPhysicalDetails height={pokemon.height} weight={pokemon.weight} />
+										<PokemonPhysicalDetails
+											height={pokemon.height}
+											weight={pokemon.weight}
+										/>
 									</Grid>
 								</Grid>
 							</CardContent>
-						</Card>
+						</CustomCard>
 					</Grid>
 
 					{/* Pokémon Stats */}
 					<Grid size={12}>
-						<Card>
+						<CustomCard>
 							<CardContent>
 								<PokemonStats stats={pokemon.stats} />
 							</CardContent>
-						</Card>
+						</CustomCard>
 					</Grid>
 				</Grid>
 			)}
